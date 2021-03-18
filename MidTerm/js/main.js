789bde21682cb646d65c3fd3b7c8b13a5b020659
ErrlyBird <56852541+ErrlyBird@users.noreply.github.com>
2021-03-18 12:56:31 -0400

    My portfolio
    all my portfolio files for the mid term

---
+++ MidTerm/js/main.js
@@ -0,0 +1,8 @@
+//allow the navigation to drop upon clicking for mobile
+
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
+
+navBarToggle.addEventListener('click', function () {
+  mainNav.classList.toggle('active');
+});
